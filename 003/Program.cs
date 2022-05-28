841afc655de9df4cd5e7f1e32f3ac3d587204b52
«Aliya <alyakalimova@mail.ru>
2022-05-28 23:19:58 +0600

    square

--- 003/Program.cs
+++ 003/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// check the square number between two numbers
+Console.Write("Enter your numberA: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Enter numberB: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA == numberB*numberB)
+{
+    Console.Write("yes, its square number of the second number");
+}
+else
+{
+    Console.Write("no, its not square number of the second number");
+}
