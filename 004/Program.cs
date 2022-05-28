245b251ba3d04c90f8912fb28114c9ccbd7186ab
«Aliya <alyakalimova@mail.ru>
2022-05-28 23:34:07 +0600

    max min numbers

---
+++ 004/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("enter your numberA: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("enter your numberB: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA>numberB)
+{
+    Console.WriteLine("numberA is bigger");
+    Console.WriteLine("numberB is less");
+}
+else if (numberA<numberB)
+{
+    Console.WriteLine("nummberA is less");
+    Console.WriteLine("numberB is bigger");
+}
+else
+{
+    Console.WriteLine("numbers are equal");
+}
